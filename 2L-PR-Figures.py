63d13af16cca5b6b820df62139d4b63ec00851b6
DCSR <dcsrobts@gmail.com>
2018-07-05 09:52:26 -0700

    Backup of 2L-PR-Figures.py

--- 2L-PR-Figures.py
+++ 2L-PR-Figures.py
@@ -48,8 +48,11 @@ class GuiClass(object):
         Button6.grid(column = 0, row = 5)
         Button7 = ttk.Button(self.buttonFrame,text="Figure 4",command=lambda arg = 1: self.figure4(arg))
         Button7.grid(column = 0, row = 6)
-        Button8 = ttk.Button(self.buttonFrame,text="Split axis test",command=lambda arg = 1: self.test(arg))
+        Button8 = ttk.Button(self.buttonFrame,text="Four Sec Bar",command=lambda arg = 1: self.fourSecBar(arg))
         Button8.grid(column = 0, row = 7)
+        
+        Button8 = ttk.Button(self.buttonFrame,text="Split axis test",command=lambda arg = 1: self.test(arg))
+        Button8.grid(column = 0, row = 8)
                
         # Canvas Frame
         self.graphFrame = ttk.Frame(self.root,borderwidth=5, relief="sunken")
@@ -68,6 +71,30 @@ class GuiClass(object):
         timeLabel.grid(column = 0, row = 0, sticky = (E))
 
     # ******************* Figure 4 Rev ************************
+
+    def fourSecBar(self,arg):
+        
+        # 4, 8, 15, 30, 60, 120, 180)
+        N = 7            
+        means = (1.368, 1.103, 0.955, 0.923, 0.997, 0.798, 0.913)
+        SEMs = (0.124, 0.129, 0.126, 0.137, 0.141, 0.128, 0.118)
+
+        bars = np.arange(N)  # the x locations for the groups
+        width = 0.45       # the width of the bars
+
+        fig, ax = plt.subplots()
+        barChart = ax.bar(bars, means, width, color='black', yerr = SEMs)
+
+        # add some text for labels, title and axes ticks
+        ax.set_ylabel('Hold-down duration in first four seconds (Sec)')
+        ax.set_title('Mean Hold-down time in first 4 seconds')
+        ax.set_xticks(bars)
+        ax.set_xticklabels(('4', '8', '15', '30', '60', '120', '180'))
+        ax.set_xlabel('Access duration (Sec)')
+
+        plt.show()
+
+        
 
     def figure4Rev(self,arg):
         """
@@ -126,23 +153,60 @@ class GuiClass(object):
 
 
         
-        y120 = []
-        y120SEM = []
+        y120 = [9.7, 257.7, 320.1, 210.7, 140.7, 96.2, 70.9, 71.2, 68.9, 81.6, \
+                67.1, 63.1, 63.6, 61.1, 67.2, 58.5, 50.1, 40.9, 42.1, 37.1, \
+                40.6, 34.5, 30.6, 29.4, 29.8, 32.4, 34.8, 39.8, 30.3, 31.1, \
+                28.0, 29.0, 20.2, 27.7, 18.4, 20.1, 20.6, 17.4, 17.0, 23.5, \
+                26.1, 20.9, 17.7, 16.5, 17.1, 14.1, 9.5, 11.9, 11.4, 14.7, \
+                9.9, 11.4, 9.5, 6.9, 13.3, 15.8, 10.6, 15.7, 12.5, 9.9, \
+                11.0, 13.4, 7.9, 10.5, 11.8, 14.0, 11.5, 6.9, 10.0, 11.2, \
+                13.9, 10.2, 12.8, 9.1, 14.5, 10.9, 8.9, 10.4, 10.2, 4.8, \
+                7.8, 6.4, 6.3, 7.0, 6.0, 6.0, 7.3, 7.4, 10.7, 8.5, \
+                6.1, 5.0, 5.1, 7.5, 12.1, 11.7, 10.8, 10.6, 8.5, 8.7, \
+                5.7, 6.3, 8.9, 8.1, 5.8, 8.2, 11.0, 13.4, 10.8, 4.6, \
+                8.9, 8.3, 10.9, 4.9, 8.1, 6.7, 9.1, 4.8, 5.3, 4.3]
+
+        y120SEM = [5.1, 69.4, 42.8, 27.6, 26.5, 20.4, 15.3, 14.8, 16.5, 14.1, \
+                   14.5, 16.8, 15.6, 14.9, 15.0, 11.9, 12.0, 10.3, 8.3, 7.4, \
+                   9.9, 9.0, 6.8, 7.6, 7.6, 5.5, 10.6, 11.4, 6.4, 7.3, \
+                   7.3, 6.6, 4.9, 7.5, 6.1, 5.9, 3.7, 4.6, 5.7, 7.1, \
+                   8.6, 6.5, 4.4, 3.8, 4.2, 4.7, 4.9, 4.5, 2.5, 4.8, \
+                   4.0, 2.8, 3.1, 2.7, 4.0, 5.0, 2.6, 4.0, 2.9, 3.4, \
+                   3.2, 2.7, 2.6, 5.1, 2.9, 4.1, 3.7, 2.1, 2.7, 3.6, \
+                   4.2, 4.0, 2.8, 2.2, 3.0, 2.4, 2.8, 3.0, 3.7, 1.9, \
+                   3.1, 2.0, 2.1, 3.2, 2.5, 2.4, 2.8, 2.3, 3.0, 2.8, \
+                   2.3, 1.7, 1.8, 2.7, 2.6, 4.3, 4.2, 3.9, 3.0, 3.4, \
+                   1.8, 3.1, 3.8, 3.6, 2.5, 2.3, 3.4, 3.0, 2.7, 2.4, \
+                   2.8, 2.1, 2.8, 1.6, 3.2, 2.7, 3.7, 1.6, 2.7, 3.5]
         
-        y60 = []
-        y60SEM = []
+        y60 = [10.4, 325.6, 379.8, 281.8, 198.5, 130.9, 96.2, 85.3, 73.9, 57.0, \
+               61.2, 52.4, 45.8, 44.7, 44.3, 45.3, 42.5, 36.0, 32.4, 34.5, \
+               34.2, 34.3, 33.7, 30.4, 29.4, 17.8, 16.8, 22.2, 23.0, 20.2, \
+               27.3, 26.3, 20.1, 26.0, 16.5, 14.3, 17.7, 18.7, 15.7, 19.6, \
+               17.0, 16.5, 16.9, 15.7, 9.3, 11.6, 11.0, 11.5, 11.6, 7.1, \
+               8.5, 12.4, 18.2, 13.8, 13.9, 11.4, 13.7, 14.0, 15.1, 18.2]
+        y60SEM = [6.0, 59.8, 53.9, 48.6, 34.2, 27.5, 21.4, 23.0, 21.0, 17.9, \
+                  22.1, 23.4, 15.5, 13.0, 13.5, 14.1, 15.4, 14.0, 14.9, 11.5, \
+                  9.0, 12.7, 14.2, 8.8, 7.8, 5.9, 6.0, 5.4, 5.0, 5.7, \
+                  8.1, 6.2, 5.0, 6.5, 5.6, 4.7, 5.3, 5.5, 3.5, 3.7, \
+                  3.3, 4.1, 3.6, 4.2, 2.4, 2.5, 3.5, 4.8, 3.6, 2.3, \
+                  2.2, 4.1, 5.0, 4.6, 4.2, 3.9, 6.8, 6.6, 8.3, 7.5]
 
-        y30 = []
-        y30SEM = []
+        y30 = [13.2, 326.4, 340.4, 243.2, 179.9, 153.8, 131.1, 130.9, 113.5, 97.5, \
+               96.6, 87.8, 76.3, 59.9, 53.5, 52.9, 48.4, 46.3, 50.5, 50.8, \
+               49.4, 49.2, 43.3, 38.8, 38.1, 34.3, 23.0, 22.0, 28.8, 27.2]
+        y30SEM = [6.5, 47.3, 58.6, 44.0, 33.4, 31.4, 31.7, 30.7, 29.6, 27.8, \
+                  23.3, 18.5, 14.9, 11.8, 16.0, 20.2, 17.4, 12.3, 12.9, 13.3, \
+                  20.9, 14.1, 14.8, 10.4, 13.6, 9.1, 5.2, 5.8, 7.4, 9.7]
 
-        y15 = []
-        y15SEM = []
+        y15 = [13.2, 328.1, 350.2, 264.0, 240.5, 208.2, 186.4, 170.2, 145.8, 134.9, 124.7, 91.4, 71.8, 63.0, 57.7]
+        y15SEM = [6.1, 58.9, 47.3, 34.8, 43.4, 44.6, 36.6, 34.9, 35.6, 31.8, 34.3, 20.6, 19.0, 20.1, 20.6]
 
-        y8 = []
-        y8SEM = []
+        y8 = [13.7, 350.8, 412.0, 327.3, 257.9, 209.6, 192.8, 153.8]
+        y8SEM = [5.9, 70.6, 50.7, 32.1, 38.7, 35.2, 35.7, 35.1]
 
-        y4 = []
-        y4SEM = []
+        y4 = [17.1, 416.1, 514.6, 420.3]
+        y4SEM = [8.2, 70.5, 46.5, 48.9]
 
         y2 = []
         y2SEM = []
@@ -170,7 +234,6 @@ class GuiClass(object):
         
         ax1.errorbar(x180, y180, y180SEM, marker = 'o', markersize = myMarkersize, linewidth = myLinewidth, \
                      color = 'black', ecolor = 'none', label = "180 sec")
-        """
         ax1.errorbar(x120, y120, y120SEM, marker = 'o', markersize = myMarkersize, linewidth = myLinewidth, \
                      color = 'red', ecolor = 'none', label = "120 sec")
         ax1.errorbar(x60, y60, y60SEM, marker = 'o', markersize = myMarkersize, linewidth = myLinewidth, \
@@ -183,6 +246,7 @@ class GuiClass(object):
                    color = 'olive', ecolor = 'none', label = "8 sec")
         ax1.errorbar(x4, y4, y4SEM, marker = 'o', markersize = myMarkersize, linewidth = myLinewidth, \
                      color = 'blue', ecolor = 'none', label = "4 sec")
+        """
         ax1.errorbar(x2, y2, y2SEM, marker = 'o', markersize = myMarkersize, linewidth = myLinewidth, \
                      color = 'plum', ecolor = 'none', label = "2 sec")
         ax1.errorbar(x1, y1, y1SEM, marker = 'o', markersize = myMarkersize, linewidth = myLinewidth, \
@@ -208,7 +272,7 @@ class GuiClass(object):
         
         # ****** Left Axis *******
         ax1.spines['left'].set_color('blue')
-        plt.ylabel('Pump duration (sec)', fontsize = 14)        
+        plt.ylabel('Pump duration (mSec per 1 sec bin)', fontsize = 14)        
         ax1.set_ylim(0, 1000.0)      
         majorLocator = MultipleLocator(100.0)
         ax1.yaxis.set_major_locator(majorLocator)
@@ -224,15 +288,14 @@ class GuiClass(object):
         ax2.spines['right'].set_color('none')        
         # X axis
         ax2.spines['bottom'].set_position(('axes', -0.02))   # Offset X axis down 0.02 
-        majorLocator = MultipleLocator(3)
+        majorLocator = MultipleLocator(2)
         ax2.xaxis.set_major_locator(majorLocator)
-        ax2.set_xlim(0, 15)
+        ax2.set_xlim(0, 12)
         # Y Axis       
         ax2.set_ylim(0, 1000.0)  
 
         ax2.errorbar(x180, y180, y180SEM, marker = 'o', markersize = myMarkersize, linewidth = myLinewidth, \
                      color = 'black', ecolor = 'black', label = "180 sec")
-        """
         ax2.errorbar(x120, y120, y120SEM, marker = 'o', markersize = myMarkersize, linewidth = myLinewidth, \
                      color = 'red', ecolor = 'red', label = "120 sec")
         ax2.errorbar(x60, y60, y60SEM, marker = 'o', markersize = myMarkersize, linewidth = myLinewidth, \
@@ -245,6 +308,7 @@ class GuiClass(object):
                    color = 'olive', ecolor = 'olive', label = "8 sec")
         ax2.errorbar(x4, y4, y4SEM, marker = 'o', markersize = myMarkersize, linewidth = myLinewidth, \
                      color = 'blue', ecolor = 'blue', label = "4 sec")
+        """
         ax2.errorbar(x2, y2, y2SEM, marker = 'o', markersize = myMarkersize, linewidth = myLinewidth, \
                      color = 'plum', ecolor = 'plum', label = "2 sec")
         ax2.errorbar(x1, y1, y1SEM, marker = 'o', markersize = myMarkersize, linewidth = myLinewidth, \
